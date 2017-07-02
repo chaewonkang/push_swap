c9b442ac99a72bb572b3fcd210ff9d8e796806ed
ljoly <ljoly@student.42.fr>
2017-07-02 17:54:01 +0200

    tests

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-/*
+
 static void		display_stacks(t_stack *e)
 {
 	int		i;
@@ -31,7 +31,7 @@ static void		display_stacks(t_stack *e)
 		i++;
 	}
 	ft_putchar('\n');
-}*/
+}
 
 void		send_op(t_stack *e, int op)
 {
@@ -58,7 +58,7 @@ void		send_op(t_stack *e, int op)
 	else if (op == RRR)
 		ft_putendl("rrr");
 //	if (DISPLAY)
-//	display_stacks(e);
+	display_stacks(e);
 	MOVES++;
 }
 
@@ -77,11 +77,11 @@ void			push_swap(t_stack *e)
 		i++;
 	}
 	TAB = bubble_sort(TAB, PARAM);
-	if (PARAM < 5)	
+//	if (PARAM < 5)	
 		simple_sort(e, MIN, MAX);
-	else if (PARAM < 11)
-		few_values_sort(e);
-	else
-		quick_sort(e);
+//	else if (PARAM < 11)
+//		few_values_sort(e);
+//	else
+//		quick_sort(e);
 	ft_printf("MOVES = %d\n", MOVES);
 }
